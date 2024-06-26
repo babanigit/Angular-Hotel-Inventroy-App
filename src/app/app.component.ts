c705bf395f86bf00fc01217a67fe1fd44fccfd2a
babanigit <Aniketgithub07>
2024-06-26 15:37:51 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,21 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
+  
+  // template
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  // template: `
+  // <h1>  hello from the inline template </h1>
+  // <p> hey there I am learning angular</p>
+  // `,
+
+  // scss
+  styleUrl: './app.component.scss',
+  // styles:[`
+  //   h1 { color:red;}
+  //   `]
 })
+
 export class AppComponent {
   title = 'hotel_inventory_app';
 }
